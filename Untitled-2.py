b878f4f30513f22f745171aad2b87d96ecd1ea09
Giovanna Volpato <140326019+Givolpato@users.noreply.github.com>
2026-05-25 17:26:26 -0300

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,12 @@
+print ("Hello!  Giovanna")
+input("How much time do you want speding in this new code project?")
+
+print ("It is about Work! Lets do together, knowing more about LitoralFashionista in code!")
+print ("The code help with two ideas: 1-seu perfil e 2-tente acertar o número da sorte de desconto")
+input("Qual seu nome?")
+input("Qual sua idade?")
+input("De onde você é?")
+print("Agora acerte o número da sorte para conseguir desconto na loja! Tire print e envie pra gente!")
+input ("Escolha um número de 0 á 10000:")
+print("Seu número tem que ser o mesmo que escolhemos!") 
+alert ( 'Bem-vindo ao jogo de Número Secreto');
